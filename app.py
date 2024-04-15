e3e41c8d43d5d959707b02e99b5c9a12749a610b
lrrrtm <lrrrtm.edu@yandex.ru>
2024-04-15 12:14:36 +0300

    initial commit

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
-import requests
 import os
-from platform import system
 
-print(system())
+import flet as ft
+
+os.environ['FLET_WEB_APP_PATH'] = '/apps/connect'
+
+
+def main(page: ft.Page):
+    page.title = "Connect"
+    page.add(ft.Text("Hello, world! Connect"))
+
+
+if __name__ == "__main__":
+    ft.app(main)
