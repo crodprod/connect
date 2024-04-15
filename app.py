76b3cc969dbdced19724fbe32788580afc6e62eb
lrrrtm <lrrrtm.edu@yandex.ru>
2024-04-15 13:06:56 +0300

    initial commit

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ import os
 
 import flet as ft
 
-os.environ['FLET_WEB_APP_PATH'] = '/apps/connect'
+os.environ['FLET_WEB_APP_PATH'] = '/connect'
 
 
 def main(page: ft.Page):
     page.title = "Connect"
-    page.add(ft.Text("Hello, world! Connect"))
+    page.add(ft.Text("Это страница для Connect"))
 
 
 if __name__ == "__main__":
