d1e805a9689d5258bed52918e8e52e8f666f2900
lrrrtm <lrrrtm.edu@yandex.ru>
2024-06-13 02:29:16 +0300

    ...

--- schedule_tasks.py
+++ schedule_tasks.py
@@ -23,28 +23,36 @@ yandex = yadiskapi.YandexAPI(os.getenv('YANDEX_REST_URL'), os.getenv('YANDEX_RES
 
 def mysql_backup():
     filename = f"crodconnect_backup_{datetime.now().strftime('%Y-%m-%d-%H-%M')}.sql"
-    filepath = f"/root/crod/{filename}"
+    filepath = f"/root/crod/backups/{filename}"
 
     command = f"mysqldump -h 127.0.0.1 -P {3310} -u {os.getenv('DB_USER')} -p{os.getenv('DB_PASSWORD')} crodconnect > {filepath}"
 
     text = f"*Статус:* ✅ создан" \
            f"\n*Файл:* {filename}"
 
+    print(filename)
+    print(filepath)
+    print(command)
     try:
+        print('start sub')
         subprocess.run(command)
 
+        print('getting response')
         response = yandex.get_upload_link(f'CROD_MEDIA/Бекапы/{filename}')
         if 'href' in response.keys():
+            print('response ok')
             yandex.upload_file(
                 url=response['href'],
                 filepath=filepath
             )
 
         else:
+            print('response error')
             text = f"*Статус:* ⛔ не создан" \
                    f"\n*Ошибка:* {response['message']}"
 
     except Exception as e:
+        print('main error')
         text = f"*Статус:* ⛔ не создан" \
                f"\n*Ошибка:* {e}"
 
@@ -54,8 +62,9 @@ def mysql_backup():
     )
 
 
-schedule.every().day.at("02:23").do(mysql_backup)
+mysql_backup()
+# schedule.every().day.at("02:23").do(mysql_backup)
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
