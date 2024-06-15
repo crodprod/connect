1997adeafea5fd5c484406584074534423f3ab66
lrrrtm <lrrrtm.edu@yandex.ru>
2024-06-15 19:35:26 +0300

    added help logic

--- flask_app.py
+++ flask_app.py
@@ -50,8 +50,7 @@ def add_ticket():
 
         if db.result['status'] == 'ok':
             response = db.data
-            print(response)
-            if response is None:
+            if not response:
                 user = "Информация в системе отсутствует"
             else:
                 user = f"{response['name']}\nРоль: {response['post_']}\nСтатус: {response['status']}"
